46363f73291feb1136582756db205001269050e1
Allan Reyes <allan_bassail@hotmail.com>
2022-08-31 23:24:10 -0700

    Update `boot` and `remote` to integrate with new app
    This commit also cleans up the files a bit.

--- config/boot.py
+++ config/boot.py
@@ -1,70 +1,87 @@
 import gc
 import image
 import lcd
-import Maix
 import time
-import os
 
 from fpioa_manager import *
-from gpio import speaker
 from Maix import FPIOA, GPIO
 
 gc.threshold(65536)
 
 
-def exception_output(e):
+# Boot pin sensor
+boot_gpio = None
+
+
+def _init():
+    global boot_gpio
+
+    lcd.init()
+    lcd.rotation(1)
+    lcd.display(image.Image("logo.jpg"))
+
+    boot_pin = 16
+    fpioa = FPIOA()
+    fpioa.set_function(boot_pin, FPIOA.GPIO7)
+
+    boot_gpio = GPIO(GPIO.GPIO7, GPIO.IN)
+
+
+def _wait_for_boot_pin_presses():
+    # Wait for user to press the boot pin
+    boot_pressed = 0
+    start_time =  time.ticks_ms()
+
+    while (time.ticks_ms() - start_time) < 500:
+        if boot_gpio.value() == 0:
+            boot_pressed += 1
+            # Every time the boot pin is pressed, add another 500ms
+            time.sleep(0.5)
+            start_time = time.ticks_ms()
+
+    print("Boot pressed: " + str(boot_pressed))
+    return boot_pressed
+
+
+def _exception_output(e):
     lcd.clear()
 
-    if str(e) == '[Errno 5] EIO':
-        e = 'EIO Error - please turn on the power switch and reboot MARK'
+    if str(e) == "[Errno 5] EIO":
+        e = "EIO Error - please turn on the power switch and reboot MARK"
     print(e)
 
     num_rows = len(str(e)) // 30 + 1
-
     for i in range(num_rows):
         lcd.draw_string(0, i * 15, str(e)[i * 30:i * 30 + 30], lcd.RED, lcd.BLACK)
 
     time.sleep(5)
 
 
-try:
-    lcd.init()
-    lcd.rotation(1)
-    lcd.display(image.Image('logo.jpg'))
+def _run():
+    try:
+        boot_pressed = _wait_for_boot_pin_presses()
 
-    boot_pressed = 0
-    boot_pin = 16
-    fpioa = FPIOA()
-    fpioa.set_function(boot_pin, FPIOA.GPIO7)
-    boot_gpio = GPIO(GPIO.GPIO7, GPIO.IN)
+        # Load default M.A.R.K. features
+        if boot_pressed == 2:
+            lcd.draw_string(0, 0, "Loading default M.A.R.K. features...", lcd.RED, lcd.BLACK)
+            time.sleep(5)
+            lcd.clear()
+            gc.collect()
+            from preloaded import *
 
-    # Wait for user to press the boot pin
-    start_time =  time.ticks_ms()
-    while (time.ticks_ms() - start_time) < 500:
-        if boot_gpio.value() == 0:
-            boot_pressed += 1
-            time.sleep(0.5)
-            start_time = time.ticks_ms()
+        # Enter remote mode to control M.A.R.K. using app
+        if boot_pressed > 2:
+            lcd.draw_string(0, 0, "Entering remote mode...", lcd.RED, lcd.BLACK)
+            time.sleep(5)
+            from remote import *
 
-    print('Boot pressed: ' + str(boot_pressed))
-
-    # Load default MARK features
-    if boot_pressed == 2:
-        lcd.draw_string(0, 0, 'Loading default MARK features...', lcd.RED, lcd.BLACK)
-        time.sleep(5)
-        lcd.clear()
+        # By default, run user-defined code (i.e. our programs)
         gc.collect()
-        from preloaded import *
+        from user import *
+    except Exception as e:
+        _exception_output(e)
+        raise
 
-    # Enter remote mode to control MARK using app
-    if boot_pressed > 2:
-        lcd.draw_string(0, 0, 'Entering remote mode...', lcd.RED, lcd.BLACK)
-        time.sleep(5)
-        from remote import *
 
-    # By default, run user-defined code (i.e. our programs!)
-    gc.collect()
-    from user import *
-except Exception as e:
-    exception_output(e)
-    raise
+_init()
+_run()
